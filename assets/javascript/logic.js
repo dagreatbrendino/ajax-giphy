4994dbe08e6393bb30e88036ac3ea2ea61d27bec
Brendan K <brendanmkelly94@gmail.com>
2019-01-12 21:47:16 -0600

    added some bootstrap styling

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     //array of topics
-    var topics = ["mario","solid snake","marcus fenix","booker dewit","gordon freeman",
+    var topics = ["mario","solid snake","marcus fenix","booker dewitt","gordon freeman",
         "pikachu","lara croft","glados","link","shodan","sonic"];
     //function that adds buttons to the dom
     var populateButtons = function(arr){
@@ -11,6 +11,7 @@ $(document).ready(function(){
             //create a jQuery button, give it the class topic, and assign the value of the current topic item to the data-query attribute and the button text
             var button = $("<button></button>");
             button.addClass("topic");
+            button.addClass("col-auto");
             button.attr("data-query",arr[i]);
             button.text(arr[i]);
             //add the new button to the dom
@@ -25,7 +26,7 @@ $(document).ready(function(){
     //function that will be used to add gifs to the dom
     var getGifs = function (term) {
         gifs=[];
-        $("#gif-container").empty();
+        $("#gif-container > div").empty();
         $.ajax({
             url: queryURL + term,
             method: "GET"
@@ -36,7 +37,7 @@ $(document).ready(function(){
             }
 
             for (var j = 0; j < gifs.length; j++) {
-                $("#gif-container").append("<div><img class=img-fluid src=https://media.giphy.com/media/" + gifs[j] + "/giphy.gif />");
+                $("#gif-container > div").append("<div class='col-6 col-md-6'><img class=img-fluid src=https://media.giphy.com/media/" + gifs[j] + "/giphy.gif />");
             }
         });
     }
