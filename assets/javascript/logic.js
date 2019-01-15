fc707ab1ef46920195419d7825be355c0227821d
Brendan K <brendanmkelly94@gmail.com>
2019-01-14 22:39:41 -0600

    changed theme to television shows, added function and animation to dial that will change to the next 'channel'

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,7 +1,9 @@
+var current;
 $(document).ready(function(){
     //array of topics
-    var topics = ["mario","solid snake","marcus fenix","booker dewitt","gordon freeman",
-        "pikachu","lara croft","glados","link","shodan","sonic"];
+    var topics = ["shameless","the simpsons","family guy","always sunny","breaking bad",
+        "dexter","bojack horseman","how i met your mother","westworld","game of thrones","the office"];
+
     //function that adds buttons to the dom
     var populateButtons = function(arr){
         //removes all buttons from the dom so there won't be duplicates
@@ -13,6 +15,7 @@ $(document).ready(function(){
             button.addClass("topic");
             button.addClass("col-auto");
             button.attr("data-query",arr[i]);
+            button.attr("id",i);
             button.text(arr[i]);
             //add the new button to the dom
             $("#buttons").append(button);
@@ -52,12 +55,13 @@ $(document).ready(function(){
     }
     //when a topic is clicked
     $(document).on("click",".topic",function(){
-        console.log("i have been called");
         //set the query to be called in the giphyapi to the title of the invoking button
         var query = $(this).attr("data-query");
         //replace the spaces in the query with +
         query.replace(" ","+");
         getGifs(query);
+        current = $(this).attr("data-query");
+        console.log(current);
     });
     //when the user clicks the submit button
     $(document).on("click","#addChar",function(event){
@@ -82,5 +86,23 @@ $(document).ready(function(){
             //otherwise change the source to the still version
             $(this).attr("src",$(this).attr("data-still"));
         }
-    })
+    });
+    $(document).on("click",".tv-turner", function(event){
+        var topicIndex = topics.indexOf(current);
+        if(topicIndex === (topics.length - 1)){
+            topicIndex =0;
+        }
+        else{
+            topicIndex ++;
+        }
+        var degrees = parseInt($(this).attr("data-deg"));
+        degrees += 45;
+        $(this).rotate({animateTo: degrees});
+        $(this).attr("data-deg",degrees);
+        current = topics[topicIndex];
+        current.replace(" ","+");
+        $("#" + topicIndex).focus();
+        getGifs(current);
+        current = topics[topicIndex];
+    });
 });
